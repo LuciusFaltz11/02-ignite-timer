a0bddb8d87977cd56233060ebe1c1282a5183cf2
LuciusFaltz11 <91973902+LuciusFaltz11@users.noreply.github.com>
2022-08-09 16:53:12 -0300

    first commit

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,36 @@
+import { Play } from 'phosphor-react';
+import {
+  CountdownContainer,
+  FormContainer,
+  HomeContainer,
+  Separator,
+} from './styles';
+
+export const Home = () => {
+  return (
+    <HomeContainer>
+      <form>
+        <FormContainer>
+          <label htmlFor="task">Vou trabalhar em</label>
+          <input id="task" type="text" />
+          <label htmlFor="minuteAmount">durante</label>
+          <input type="number" id="minuteAmount" />
+          <span>minutos</span>
+        </FormContainer>
+
+        <CountdownContainer>
+          <span>0</span>
+          <span>0</span>
+          <Separator>:</Separator>
+          <span>0</span>
+          <span>0</span>
+        </CountdownContainer>
+
+        <button type="submit">
+          <Play size={24} />
+          Começar
+        </button>
+      </form>
+    </HomeContainer>
+  );
+};
